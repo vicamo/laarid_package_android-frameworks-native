46c07a1b3ec4364390382b940f90010dfadd75b6
You-Sheng Yang <vicamo@gmail.com>
2017-03-26 11:11:23 +0800

    build libinputflinger

--- services/inputflinger/Android.mk
+++ services/inputflinger/Android.mk
@@ -12,38 +12,71 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+lib_LTLIBRARIES += \
+    %reldir%/libandroid-inputflinger.la
 
-LOCAL_SRC_FILES:= \
-    EventHub.cpp \
-    InputApplication.cpp \
-    InputDispatcher.cpp \
-    InputListener.cpp \
-    InputManager.cpp \
-    InputReader.cpp \
-    InputWindow.cpp
+%canon_reldir%_libandroid_inputflinger_la_SOURCES = \
+    %reldir%/EventHub.cpp \
+    %reldir%/EventHub.h \
+    %reldir%/InputApplication.cpp \
+    %reldir%/InputApplication.h \
+    %reldir%/InputDispatcher.cpp \
+    %reldir%/InputDispatcher.h \
+    %reldir%/InputListener.cpp \
+    %reldir%/InputListener.h \
+    %reldir%/InputManager.cpp \
+    %reldir%/InputManager.h \
+    %reldir%/InputReader.cpp \
+    %reldir%/InputReader.h \
+    %reldir%/InputWindow.cpp \
+    %reldir%/InputWindow.h \
+    %reldir%/PointerControllerInterface.h
 
-LOCAL_SHARED_LIBRARIES := \
-    libbinder \
-    libcrypto \
-    libcutils \
-    libinput \
-    liblog \
-    libutils \
-    libui \
-    libhardware_legacy
+%canon_reldir%_libandroid_inputflinger_la_LIBADD = \
+    $(CRYPTO_LIBS) \
+    $(CUTILS_LIBS) \
+    $(LOG_LIBS) \
+    $(HARDWARE_LEGACY_LIBS) \
+    $(UTILS_LIBS) \
+    libs/input/libandroid-input.la \
+    libs/ui/libandroid-ui.la
 
+%canon_reldir%_libandroid_inputflinger_la_CPPFLAGS = \
+    $(AM_CPPFLAGS) \
+    $(CRYPTO_CFLAGS) \
+    $(CUTILS_CFLAGS) \
+    $(LOG_CFLAGS) \
+    $(HARDWARE_CFLAGS) \
+    $(HARDWARE_LEGACY_CFLAGS) \
+    $(UTILS_CFLAGS)
 
+%canon_reldir%_libandroid_inputflinger_la_CXXFLAGS = \
+    $(AM_CXXFLAGS)
 # TODO: Move inputflinger to its own process and mark it hidden
-#LOCAL_CFLAGS += -fvisibility=hidden
+#%canon_reldir%_libandroid_inputflinger_la_CXXFLAGS += \
+#    -fvisibility=hidden
 
-LOCAL_CFLAGS += -Wno-unused-parameter
+%canon_reldir%_libandroid_inputflinger_la_CXXFLAGS += \
+    -Wno-unused-parameter \
+    -Wno-unused-variable \
+    -Wno-unused-but-set-variable
 
-LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
+%canon_reldir%_libandroid_inputflinger_la_LDFLAGS = \
+    $(AM_LDFLAGS) \
+    $(libtool_opts)
 
-LOCAL_MODULE := libinputflinger
+%canon_reldir%_inputflingerincdir = $(frameworks_native_incdir)/inputflinger
+%canon_reldir%_inputflingerinc_HEADERS = \
+    %reldir%/EventHub.h \
+    %reldir%/InputApplication.h \
+    %reldir%/InputDispatcher.h \
+    %reldir%/InputListener.h \
+    %reldir%/InputManager.h \
+    %reldir%/InputReader.h \
+    %reldir%/InputWindow.h \
+    %reldir%/PointerControllerInterface.h
 
-include $(BUILD_SHARED_LIBRARY)
+pkgconfig_DATA += \
+    %reldir%/android-inputflinger-0.0.pc
 
-include $(call all-makefiles-under,$(LOCAL_PATH))
+#include $(call all-makefiles-under,$(LOCAL_PATH))
