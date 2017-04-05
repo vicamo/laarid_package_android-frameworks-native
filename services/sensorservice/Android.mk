d9df88cb660d795c8a0011f2c105756c5685b113
You-Sheng Yang <vicamo@gmail.com>
2017-04-05 14:39:39 +0000

    build sensorservice

--- services/sensorservice/Android.mk
+++ services/sensorservice/Android.mk
@@ -1,62 +1,102 @@
-LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
+lib_LTLIBRARIES += \
+    %reldir%/libandroid-sensorservice.la
 
-LOCAL_SRC_FILES:= \
-    BatteryService.cpp \
-    CorrectedGyroSensor.cpp \
-    Fusion.cpp \
-    GravitySensor.cpp \
-    LinearAccelerationSensor.cpp \
-    OrientationSensor.cpp \
-    RecentEventLogger.cpp \
-    RotationVectorSensor.cpp \
-    SensorDevice.cpp \
-    SensorEventConnection.cpp \
-    SensorFusion.cpp \
-    SensorInterface.cpp \
-    SensorList.cpp \
-    SensorRecord.cpp \
-    SensorService.cpp \
-    SensorServiceUtils.cpp \
+%canon_reldir%_libandroid_sensorservice_la_SOURCES = \
+    %reldir%/BatteryService.cpp \
+    %reldir%/BatteryService.h \
+    %reldir%/CorrectedGyroSensor.cpp \
+    %reldir%/CorrectedGyroSensor.h \
+    %reldir%/Fusion.cpp \
+    %reldir%/Fusion.h \
+    %reldir%/GravitySensor.cpp \
+    %reldir%/GravitySensor.h \
+    %reldir%/LinearAccelerationSensor.cpp \
+    %reldir%/LinearAccelerationSensor.h \
+    %reldir%/OrientationSensor.cpp \
+    %reldir%/OrientationSensor.h \
+    %reldir%/RecentEventLogger.cpp \
+    %reldir%/RecentEventLogger.h \
+    %reldir%/RingBuffer.h \
+    %reldir%/RotationVectorSensor.cpp \
+    %reldir%/RotationVectorSensor.h \
+    %reldir%/SensorDevice.cpp \
+    %reldir%/SensorDevice.h \
+    %reldir%/SensorEventAckReceiver.h \
+    %reldir%/SensorEventConnection.cpp \
+    %reldir%/SensorEventConnection.h \
+    %reldir%/SensorFusion.cpp \
+    %reldir%/SensorFusion.h \
+    %reldir%/SensorInterface.cpp \
+    %reldir%/SensorInterface.h \
+    %reldir%/SensorList.cpp \
+    %reldir%/SensorList.h \
+    %reldir%/SensorRecord.cpp \
+    %reldir%/SensorRecord.h \
+    %reldir%/SensorRegistrationInfo.h \
+    %reldir%/SensorService.cpp \
+    %reldir%/SensorService.h \
+    %reldir%/SensorServiceUtils.cpp \
+    %reldir%/SensorServiceUtils.h \
+    %reldir%/mat.h \
+    %reldir%/quat.h \
+    %reldir%/traits.h \
+    %reldir%/vec.h
 
 
-LOCAL_CFLAGS:= -DLOG_TAG=\"SensorService\"
+%canon_reldir%_libandroid_sensorservice_la_CPPFLAGS = \
+    $(AM_CPPFLAGS) \
+    $(LOG_CFLAGS) \
+    $(CUTILS_CFLAGS) \
+    $(HARDWARE_CFLAGS) \
+    $(HARDWARE_LEGACY_CFLAGS) \
+    $(NATIVEHELPER_CFLAGS) \
+    $(UTILS_CFLAGS) \
+    $(BINDER_CFLAGS) \
+    $(UI_CFLAGS) \
+    -DLOG_TAG=\"SensorService\"
 
-LOCAL_CFLAGS += -Wall -Werror -Wextra
+%canon_reldir%_libandroid_sensorservice_la_CXXFLAGS = \
+    $(AM_CXXFLAGS) \
+    -fvisibility=hidden \
+    -Wno-multichar \
+    -Wno-unused-but-set-variable
 
-LOCAL_CFLAGS += -fvisibility=hidden
+%canon_reldir%_libandroid_sensorservice_la_LIBADD = \
+    $(CRYPTO_LIBS) \
+    $(CUTILS_LIBS) \
+    $(HARDWARE_LIBS) \
+    $(HARDWARE_LEGACY_LIBS) \
+    $(UTILS_LIBS) \
+    $(LOG_LIBS) \
+    $(BINDER_LIBS) \
+    libs/gui/libandroid-gui.la
 
-LOCAL_SHARED_LIBRARIES := \
-    libcutils \
-    libhardware \
-    libhardware_legacy \
-    libutils \
-    liblog \
-    libbinder \
-    libui \
-    libgui \
-    libcrypto
+%canon_reldir%_libandroid_sensorservice_la_LDFLAGS = \
+    $(AM_LDFLAGS) \
+    $(libtool_opts)
 
-LOCAL_MODULE:= libsensorservice
+pkgconfig_DATA += \
+    %reldir%/android-sensorservice-$(LAARID_API_VERSION).pc
 
-include $(BUILD_SHARED_LIBRARY)
+%canon_reldir%_sensorserviceincdir = $(frameworks_native_incdir)/sensorservice
+%canon_reldir%_sensorserviceinc_HEADERS = \
+    %reldir%/SensorService.h
 
 #####################################################################
 # build executable
-include $(CLEAR_VARS)
+bin_PROGRAMS += \
+    %reldir%/sensorservice
 
-LOCAL_SRC_FILES:= \
-    main_sensorservice.cpp
+%canon_reldir%_sensorservice_CPPFLAGS = \
+    $(AM_CPPFLAGS) \
+    $(UTILS_CFLAGS) \
+    $(BINDER_CFLAGS) \
+    $(UI_CFLAGS)
 
-LOCAL_SHARED_LIBRARIES := \
-    libsensorservice \
-    libbinder \
-    libutils
+%canon_reldir%_sensorservice_SOURCES = \
+    %reldir%/main_sensorservice.cpp
 
-LOCAL_CFLAGS := -Wall -Werror -Wextra
-
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_MODULE:= sensorservice
-
-include $(BUILD_EXECUTABLE)
+%canon_reldir%_sensorservice_LDADD = \
+    $(BINDER_LIBS) \
+    $(UTILS_LIBS) \
+    %reldir%/libandroid-sensorservice.la
